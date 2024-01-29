80fdc3e291a635142b023b874d8aa9e37a67cb08
jenny-berggreen <jenny@berggreen.no>
2024-01-29 11:51:40 +0100

    Display film titles

--- js/films.js
+++ js/films.js
@@ -2,3 +2,26 @@ const fetchSWdata = ()=> {
 	axios.get('https://swapi.dev/api/films/')
 	.then(data => displayFilmData(data.data.results))
 };
+
+const displayFilmData = (data) => {
+	console.log(data);
+	const headerLists = document.querySelectorAll('.header-list');
+	const dataLists = document.querySelectorAll('.data-list');
+	const records = document.querySelectorAll('.grid-item__record');
+	let numberOfRecords = records.length;
+
+	for (let i = 0; i < numberOfRecords; i++) {
+		let filmTitle = data[i].title;
+
+		displayTitle(filmTitle, i, headerLists);
+	}
+	
+};
+  
+const displayTitle = (title, index, headerLists) => {
+const listItem = document.createElement('li');
+listItem.innerText = "Title: " + title;
+headerLists[index].appendChild(listItem);
+};
+
+fetchSWdata();
