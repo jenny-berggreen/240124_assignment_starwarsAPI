78386fd73a2262691659df07c783ecf8b0b856db
jenny-berggreen <jenny@berggreen.no>
2024-01-31 11:45:22 +0100

    Display people name

--- js/people.js
+++ js/people.js
@@ -5,6 +5,22 @@ const fetchSWdata = ()=> {
 
 const displayPeopleData = (data) => {
 	console.log(data);
-}
+	const headerLists = document.querySelectorAll('.header-list');
+	const dataLists = document.querySelectorAll('.data-list');
+	const records = document.querySelectorAll('.grid-item__record');
+	let numberOfRecords = records.length;
+
+	for (let i = 0; i < numberOfRecords; i++) {
+		let personName = data[i].name;
+
+		displayName(personName, i, headerLists);
+	}
+};
+
+const displayName = (name, index, headerLists) => {
+	const listItem = document.createElement('li');
+	listItem.innerText = "Name: " + name;
+	headerLists[index].appendChild(listItem);
+};
 
 fetchSWdata();
