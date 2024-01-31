990e734f6142bd33fa829b76f4561b2edd252f31
jenny-berggreen <jenny@berggreen.no>
2024-01-31 12:08:19 +0100

    Display planets name

--- js/planets.js
+++ js/planets.js
@@ -5,6 +5,22 @@ const fetchSWdata = ()=> {
 
 const displayPlanetsData = (data) => {
 	console.log(data);
+	const headerLists = document.querySelectorAll('.header-list');
+	const dataLists = document.querySelectorAll('.data-list');
+	const records = document.querySelectorAll('.grid-item__record');
+	let numberOfRecords = records.length;
+
+	for (let i = 0; i < numberOfRecords; i++) {
+		let planetName = data[i].name;
+
+		displayName(planetName, i, headerLists);
+	}
+};
+
+const displayName = (name, index, headerLists) => {
+	const listItem = document.createElement('li');
+	listItem.innerText = "Name: " + name;
+	headerLists[index].appendChild(listItem);
 };
 
 fetchSWdata();
