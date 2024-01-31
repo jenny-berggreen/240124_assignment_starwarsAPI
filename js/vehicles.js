3b3aa82cf548695ad1e9c2e7e939c74de4011d26
jenny-berggreen <jenny@berggreen.no>
2024-01-31 12:45:59 +0100

    Display vehicles model

--- js/vehicles.js
+++ js/vehicles.js
@@ -12,8 +12,10 @@ const displayVehiclesData = (data) => {
 
 	for (let i = 0; i < numberOfRecords; i++) {
 		let vehicleName = data[i].name; 
+		let vehicleModel = data[i].model; 
 
 		displayName(vehicleName, i, headerLists);
+		displayModel(vehicleModel, i, dataLists);
 	}
 };
 
@@ -21,6 +23,12 @@ const displayName = (name, index, headerLists) => {
 	const listItem = document.createElement('li');
 	listItem.innerText = "Name: " + name;
 	headerLists[index].appendChild(listItem);
+};
+
+const displayModel = (model, index, dataLists) => {
+	const listItem = document.createElement('li');
+	listItem.innerText = "Model: " + model;
+	dataLists[index].appendChild(listItem);
 };
 
 fetchSWdata();
