dde86c3968dd6c02c98f6ba971fc3ee294990d55
jenny-berggreen <jenny@berggreen.no>
2024-02-08 12:20:30 +0100

    Make index divs keyboard accessible

--- js/index.js
+++ js/index.js
@@ -3,18 +3,26 @@ const peopleDiv = document.querySelector('#people');
 const planetsDiv = document.querySelector('#planets');
 const vehiclesDiv = document.querySelector('#vehicles');
 
-filmsDiv.addEventListener('click', function() {
-	window.location.href = '../films.html';
-});
+// function to handle click or keypress events
+const navigateToPage = (pageUrl) => {
+    window.location.href = pageUrl;
+};
 
-peopleDiv.addEventListener('click', function() {
-	window.location.href = '../people.html';
-});
+// function to handle keydown event
+const handleKeyDown = (event, pageUrl) => {
+    if (event.key === 'Enter') {
+        navigateToPage(pageUrl);
+    }
+};
 
-planetsDiv.addEventListener('click', function() {
-	window.location.href = '../planets.html';
-});
+// click event listeners
+filmsDiv.addEventListener('click', () => navigateToPage('../films.html'));
+peopleDiv.addEventListener('click', () => navigateToPage('../people.html'));
+planetsDiv.addEventListener('click', () => navigateToPage('../planets.html'));
+vehiclesDiv.addEventListener('click', () => navigateToPage('../vehicles.html'));
 
-vehiclesDiv.addEventListener('click', function() {
-	window.location.href = '../vehicles.html';
-});
+// keyboard event listeners
+filmsDiv.addEventListener('keydown', (event) => handleKeyDown(event, '../films.html'));
+peopleDiv.addEventListener('keydown', (event) => handleKeyDown(event, '../people.html'));
+planetsDiv.addEventListener('keydown', (event) => handleKeyDown(event, '../planets.html'));
+vehiclesDiv.addEventListener('keydown', (event) => handleKeyDown(event, '../vehicles.html'));
